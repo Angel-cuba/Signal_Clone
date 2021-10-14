079cdac7fca64bfb4deaf8958c5fb5cf08b0b8d9
Angel <araozangel842@gmail.com>
2021-10-14 09:24:15 +0300

    Fixing WARNING

--- App.js
+++ App.js
@@ -18,6 +18,50 @@ const globalScreenOptions={
 }
 
 export default function App() {
+
+  //Funcion para arreglar el WARNING
+  const _setTimeout = global.setTimeout;
+const _clearTimeout = global.clearTimeout;
+const MAX_TIMER_DURATION_MS = 60 * 1000;
+
+if (Platform.OS === "android") {
+    const timerFix = {};
+    const runTask = (id, fn, ttl, args) => {
+        const waitingTime = ttl - Date.now();
+        if (waitingTime <= 1) {
+            InteractionManager.runAfterInteractions(() => {
+                if (!timerFix[id]) {
+                    return;
+                }
+                delete timerFix[id];
+                fn(...args);
+            });
+            return;
+        }
+
+        const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+        timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+    };
+
+    global.setTimeout = (fn, time, ...args) => {
+        if (MAX_TIMER_DURATION_MS < time) {
+            const ttl = Date.now() + time;
+            const id = "_lt_" + Object.keys(timerFix).length;
+            runTask(id, fn, ttl, args);
+            return id;
+        }
+        return _setTimeout(fn, time, ...args);
+    };
+
+    global.clearTimeout = (id) => {
+        if (typeof id === "string" && id.startsWith("_lt_")) {
+            _clearTimeout(timerFix[id]);
+            delete timerFix[id];
+            return;
+        }
+        _clearTimeout(id);
+    };
+}
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={globalScreenOptions}>
@@ -35,6 +79,7 @@ export default function App() {
     </NavigationContainer>
   
   );
+  
 }
 
 const styles = StyleSheet.create({
