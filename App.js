eec90383de49265db023badbc6d369f4aafbf437
Angel <araozangel842@gmail.com>
2021-10-14 15:22:56 +0300

    Fixed the warning issue

--- App.js
+++ App.js
@@ -1,92 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import HomeScreen from './screens/HomeScreen'
+import { StyleSheet, InteractionManager } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
 import AddChatScreen from './screens/AddChatScreen';
-import ChatScreen from './screens/ChatScreen'
+import ChatScreen from './screens/ChatScreen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-const globalScreenOptions={
-  headerStyle: { backgroundColor: '#2C6BED'},
-  headerTitleStyle: { color: 'silver'},
-  headerTintColor: { color: 'silver'}
-}
+const globalScreenOptions = {
+	headerStyle: { backgroundColor: '#2C6BED' },
+	headerTitleStyle: { color: 'silver' },
+	headerTintColor: { color: 'silver' },
+};
 
 export default function App() {
+	//Funcion para arreglar el WARNING
+	const _setTimeout = global.setTimeout;
+	const _clearTimeout = global.clearTimeout;
+	const MAX_TIMER_DURATION_MS = 60 * 1000;
 
-  //Funcion para arreglar el WARNING
-  const _setTimeout = global.setTimeout;
-const _clearTimeout = global.clearTimeout;
-const MAX_TIMER_DURATION_MS = 60 * 1000;
+	if (Platform.OS === 'android') {
+		const timerFix = {};
+		const runTask = (id, fn, ttl, args) => {
+			const waitingTime = ttl - Date.now();
 
-if (Platform.OS === "android") {
-    const timerFix = {};
-    const runTask = (id, fn, ttl, args) => {
-        const waitingTime = ttl - Date.now();
-        if (waitingTime <= 1) {
-            InteractionManager.runAfterInteractions(() => {
-                if (!timerFix[id]) {
-                    return;
-                }
-                delete timerFix[id];
-                fn(...args);
-            });
-            return;
-        }
+			if (waitingTime <= 1) {
+				InteractionManager.runAfterInteractions(() => {
+					if (!timerFix[id]) {
+						return;
+					}
+					delete timerFix[id];
+					fn(...args);
+				});
+				return;
+			}
 
-        const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
-        timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
-    };
+			const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+			timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+		};
 
-    global.setTimeout = (fn, time, ...args) => {
-        if (MAX_TIMER_DURATION_MS < time) {
-            const ttl = Date.now() + time;
-            const id = "_lt_" + Object.keys(timerFix).length;
-            runTask(id, fn, ttl, args);
-            return id;
-        }
-        return _setTimeout(fn, time, ...args);
-    };
+		global.setTimeout = (fn, time, ...args) => {
+			if (MAX_TIMER_DURATION_MS < time) {
+				const ttl = Date.now() + time;
+				const id = '_lt_' + Object.keys(timerFix).length;
+				runTask(id, fn, ttl, args);
+				return id;
+			}
+			return _setTimeout(fn, time, ...args);
+		};
 
-    global.clearTimeout = (id) => {
-        if (typeof id === "string" && id.startsWith("_lt_")) {
-            _clearTimeout(timerFix[id]);
-            delete timerFix[id];
-            return;
-        }
-        _clearTimeout(id);
-    };
-}
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={globalScreenOptions}>
-        <Stack.Screen options={{
-          title: 'Login',
-        }} name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Register" component={RegisterScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="AddChat" component={AddChatScreen}/>
-        <Stack.Screen name="Chat" component={ChatScreen}/>
-
-
-      </Stack.Navigator>
-       
-    </NavigationContainer>
-  
-  );
-  
+		global.clearTimeout = (id) => {
+			if (typeof id === 'string' && id.startsWith('_lt_')) {
+				_clearTimeout(timerFix[id]);
+				delete timerFix[id];
+				return;
+			}
+			_clearTimeout(id);
+		};
+	}
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={globalScreenOptions}>
+				<Stack.Screen
+					options={{
+						title: 'Login',
+					}}
+					name="Login"
+					component={LoginScreen}
+				/>
+				<Stack.Screen name="Register" component={RegisterScreen} />
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="AddChat" component={AddChatScreen} />
+				<Stack.Screen name="Chat" component={ChatScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
