fcab320dc2384d4a19f49f30fd2f7b37980cd741
Angel <araozangel842@gmail.com>
2021-10-14 15:11:56 +0300

    Fixing button disability if input field is empty

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,19 +1,22 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { Icon, Input } from 'react-native-elements';
-import {db } from '../firebase/firebase'
+import { db } from '../firebase/firebase';
 
 const AddChatScreen = ({ navigation }) => {
 	const [input, setInput] = useState('');
 
-	const createChat = async() => {
-          await db.collection('chat').add({
-               chatName: input
-          }).then(() => {
-               navigation.goBack()
-          })
-          .catch((error) => Alert.alert(error.message))
-     };
+	const createChat = async () => {
+		await db
+			.collection('chat')
+			.add({
+				chatName: input,
+			})
+			.then(() => {
+				navigation.goBack();
+			})
+			.catch((error) => Alert.alert(error.message));
+	};
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -28,10 +31,10 @@ const AddChatScreen = ({ navigation }) => {
 				placeholder="Enter a new chat"
 				value={input}
 				onChangeText={(text) => setInput(text)}
-                    onSubmitEditing={createChat}
+				onSubmitEditing={createChat}
 				leftIcon={<Icon name="wechat" type="antdesign" size={24} color="black" />}
 			/>
-               <Button onPress={createChat} title="Create a new chat"/>
+			<Button onPress={createChat} disabled={!input} title="Create a new chat" />
 		</View>
 	);
 };
