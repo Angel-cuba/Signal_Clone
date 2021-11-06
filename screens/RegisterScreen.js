8cc92d89b60660ac61387cb7c22496435992b751
Angel <araozangel842@gmail.com>
2021-11-06 15:51:56 +0200

    Adding animation, Alert if email is duplicate

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useLayoutEffect } from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, Image } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, Image, Alert } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { firebase } from '../firebase/firebase';
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import * as ImagePicker from 'expo-image-picker';
-import { Alert } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 const RegisterScreen = ({ navigation }) => {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [imageUrl, setImageUrl] = useState('');
+	const [loading, setLoading] = useState(false);
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -21,6 +22,7 @@ const RegisterScreen = ({ navigation }) => {
 	}, [navigation]);
 
 	const register = () => {
+		setLoading(true);
 		firebase
 			.auth()
 			.createUserWithEmailAndPassword(email, password)
@@ -32,7 +34,24 @@ const RegisterScreen = ({ navigation }) => {
 						'https://res.cloudinary.com/dqaerysgb/image/upload/v1630358737/jooly8uzpykfvixik2vv.jpg',
 				});
 			})
-			.catch((error) => alert(error.message));
+			.then(() => {
+				setTimeout(() => {
+					setLoading(false);
+				}, 2000);
+			})
+			.catch((error) =>
+				Alert.alert('Yooo dude', error.message + '\n\n... I think that u should change it 👀', [
+					{
+						text: 'Ok...😘',
+						onPress: () => console.log('Ok'),
+						style: 'cancel',
+					},
+					{
+						text: 'Try again',
+						onPress: () => navigation.push('Register'),
+					},
+				])
+			);
 	};
 
 	const uploadPictureWithCamera = async () => {
@@ -77,67 +96,79 @@ const RegisterScreen = ({ navigation }) => {
 	};
 
 	return (
-		<KeyboardAvoidingView behavior="padding" style={styles.container}>
-			<StatusBar style="light" />
+		<>
+			<KeyboardAvoidingView behavior="padding" style={styles.container}>
+				<StatusBar style="light" />
 
-			<Text style={styles.titleText}>Create a new account</Text>
+				<Text style={styles.titleText}>Create a new account</Text>
 
-			<View style={styles.inputContainer}>
-				<Input
-					placeholder="Full Name"
-					autoFocus
-					type="text"
-					value={name}
-					onChangeText={(text) => setName(text)}
-				/>
-				<Input
-					placeholder="Email"
-					type="email"
-					value={email}
-					onChangeText={(text) => setEmail(text)}
-				/>
-				<Input
-					placeholder="Password"
-					type="password"
-					secureTextEntry
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				/>
-				<Input
-					placeholder="Profile Picture URL (optional)"
-					type="text"
-					value={imageUrl}
-					onChangeText={(text) => setImageUrl(text)}
-					onSubmitEditing={register}
-				/>
+				<View style={styles.inputContainer}>
+					<Input
+						placeholder="Full Name"
+						autoFocus
+						type="text"
+						value={name}
+						onChangeText={(text) => setName(text)}
+					/>
+					<Input
+						placeholder="Email"
+						type="email"
+						value={email}
+						onChangeText={(text) => setEmail(text)}
+					/>
+					<Input
+						placeholder="Password"
+						type="password"
+						secureTextEntry
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+					/>
+					<Input
+						placeholder="Profile Picture URL (optional)"
+						type="text"
+						value={imageUrl}
+						onChangeText={(text) => setImageUrl(text)}
+						onSubmitEditing={register}
+					/>
 
-				{imageUrl !== '' && (
-					<View style={styles.image}>
-						<Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
+					{imageUrl !== '' && (
+						<View style={styles.image}>
+							<Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
+						</View>
+					)}
+				</View>
+
+				<View style={styles.buttonsBroup}>
+					<Button
+						// raised
+						icon={<Icon name="camera" size={24} color="black" />}
+						styles={styles.button}
+						onPress={uploadPictureWithCamera}
+						// iconPosition="right"
+						title="Camera"
+					/>
+					<Button
+						raised
+						styles={styles.button}
+						onPress={uploadPictureFromPhone}
+						icon={<Icon name="cellphone-arrow-down" size={24} color="black" />}
+						title="Phone"
+					/>
+				</View>
+
+				<Button raised styles={styles.button} onPress={register} title="Register" />
+				{loading && (
+					<View style={styles.loadingScreen}>
+						<LottieView
+							source={require('../assets/animations/82472-loading.json')}
+							autoPlay
+							style={{ height: 300 }}
+							speed={2}
+						/>
 					</View>
 				)}
-			</View>
-
-			<View style={styles.buttonsBroup}>
-				<Button
-					// raised
-					icon={<Icon name="camera" size={24} color="black" />}
-					styles={styles.button}
-					onPress={uploadPictureWithCamera}
-					// iconPosition="right"
-					title="Camera"
-				/>
-				<Button
-					raised
-					styles={styles.button}
-					onPress={uploadPictureFromPhone}
-					icon={<Icon name="cellphone-arrow-down" size={24} color="black" />}
-					title="Phone"
-				/>
-			</View>
-
-			<Button raised styles={styles.button} onPress={register} title="Register" />
-		</KeyboardAvoidingView>
+			</KeyboardAvoidingView>
+		</>
 	);
 };
 
@@ -173,5 +204,15 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		width: 200,
+	},
+	loadingScreen: {
+		position: 'absolute',
+		backgroundColor: 'black',
+		opacity: 0.6,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '100%',
+		height: '100%',
+		zIndex: 1000,
 	},
 });
